9b291fe6c0bf9507f1887d1707eeddc3e87051a0
matarata94 <mostafafahimi73@gmail.com>
2017-02-22 10:40:24 +0330

    Initial commit

---
+++ app/src/main/java/technoto/ir/beautyshop/MainActivity.java
@@ -0,0 +1,13 @@
+package technoto.ir.beautyshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
